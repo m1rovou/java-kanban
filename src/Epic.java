1aa338cb0e94107c3b94e9160270c59c5729406a
АSit <shuvaev.a.m@mail.ru>
2024-01-15 03:04:29 +0300

    SecondGeneration/

--- src/Epic.java
+++
@@ -1,5 +0,0 @@
-public class Epic extends Task {
-    public Epic(int id, String title, String description, TaskStatus status) {
-        super(id, title, description, status);
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+ import my.directory.kanban.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,22 +8,20 @@ public class Main {
         Task task2 = taskManager.createTask("Задача 2", "Описание для Задачи 2", TaskStatus.IN_PROGRESS);
 
         Epic epic1 = taskManager.createEpic("Эпик 1", "Описание для Эпика 1", TaskStatus.NEW);
-
-        Subtask subtask1 = taskManager.createSubtask(epic1.getId(), "Подзадача 1", "Описание для Подзадачи 1", TaskStatus.DONE);
-        Subtask subtask2 = taskManager.createSubtask(epic1.getId(), "Подзадача 2", "Описание для Подзадачи 2", TaskStatus.IN_PROGRESS);
-
         Epic epic2 = taskManager.createEpic("Эпик 2", "Описание для Эпика 2", TaskStatus.IN_PROGRESS);
-
-        Subtask subtask3 = taskManager.createSubtask(epic2.getId(), "Подзадача 3", "Описание для Подзадачи 3", TaskStatus.DONE);
-        Subtask subtask4 = taskManager.createSubtask(epic2.getId(), "Подзадача 4", "Описание для Подзадачи 4", TaskStatus.IN_PROGRESS);
-
         Epic epic3 = taskManager.createEpic("Эпик 3", "Описание для Эпика 3", TaskStatus.IN_PROGRESS);
         Epic epic4 = taskManager.createEpic("Эпик 4", "Описание для Эпика 4", TaskStatus.IN_PROGRESS);
 
+
+        Subtask subtask1 = taskManager.createSubtask(epic1.getId(), "Подзадача 1", "Описание для Подзадачи 1", TaskStatus.DONE);
+        Subtask subtask2 = taskManager.createSubtask(epic1.getId(), "Подзадача 2", "Описание для Подзадачи 2", TaskStatus.IN_PROGRESS);
+        Subtask subtask3 = taskManager.createSubtask(epic2.getId(), "Подзадача 3", "Описание для Подзадачи 3", TaskStatus.DONE);
+        Subtask subtask4 = taskManager.createSubtask(epic2.getId(), "Подзадача 4", "Описание для Подзадачи 4", TaskStatus.IN_PROGRESS);
         Subtask subtask5 = taskManager.createSubtask(epic3.getId(), "Подзадача 5", "Описание для Подзадачи 5", TaskStatus.DONE);
         Subtask subtask6 = taskManager.createSubtask(epic3.getId(), "Подзадача 6", "Описание для Подзадачи 6", TaskStatus.IN_PROGRESS);
         Subtask subtask7 = taskManager.createSubtask(epic4.getId(), "Подзадача 7", "Описание для Подзадачи 7", TaskStatus.DONE);
         Subtask subtask8 = taskManager.createSubtask(epic4.getId(), "Подзадача 8", "Описание для Подзадачи 8", TaskStatus.IN_PROGRESS);
+
 
 
         System.out.println("Все задачи:");
@@ -40,30 +37,77 @@ public class Main {
 
 
         System.out.println("Подзадачи для Эпика 1:");
-        HashMap<Integer, Subtask> subtasksForEpic1 = taskManager.getSubtasksByEpicId(epic1.getId());
-        ArrayList<Subtask> subtasksListForEpic1 = new ArrayList<>(subtasksForEpic1.values());
-        for (Subtask subtask : subtasksListForEpic1) {
+        for (Subtask subtask : taskManager.getSubtasksByEpicId(epic1.getId()).values()) {
             System.out.println(subtask);
         }
+
         System.out.println("Подзадачи для Эпика 3:");
-        HashMap<Integer, Subtask> subtasksForEpic3 = taskManager.getSubtasksByEpicId(epic3.getId());
-        ArrayList<Subtask> subtasksListForEpic3 = new ArrayList<>(subtasksForEpic3.values());
-        for (Subtask subtask : subtasksListForEpic3) {
+        for (Subtask subtask : taskManager.getSubtasksByEpicId(epic3.getId()).values()) {
             System.out.println(subtask);
         }
+
         System.out.println("Подзадачи для Эпика 4:");
-        HashMap<Integer, Subtask> subtasksForEpic4 = taskManager.getSubtasksByEpicId(epic4.getId());
-        ArrayList<Subtask> subtasksListForEpic4 = new ArrayList<>(subtasksForEpic4.values());
-        for (Subtask subtask : subtasksListForEpic4) {
+        for (Subtask subtask : taskManager.getSubtasksByEpicId(epic4.getId()).values()) {
             System.out.println(subtask);
         }
 
         System.out.println("Подзадачи для Эпика 2:");
-        HashMap<Integer, Subtask> subtasksForEpic2 = taskManager.getSubtasksByEpicId(epic2.getId());
-        ArrayList<Subtask> subtasksListForEpic2 = new ArrayList<>(subtasksForEpic2.values());
-        for (Subtask subtask : subtasksListForEpic2) {
+        for (Subtask subtask : taskManager.getSubtasksByEpicId(epic2.getId()).values()) {
             System.out.println(subtask);
         }
+
+        System.out.println(taskManager.getTaskById(task1.getId()));
+        System.out.println(taskManager.getTaskById(task2.getId()));
+
+        int subtaskIdToCheck = 8;
+        Subtask retrievedSubtask = taskManager.getSubtaskById(subtaskIdToCheck);
+        System.out.println("Подзадача " + retrievedSubtask + " найдена:");
+
+        subtaskIdToCheck = 13;
+        retrievedSubtask = taskManager.getSubtaskById(subtaskIdToCheck);
+        System.out.println("Подзадача  " + retrievedSubtask + " найдена:");
+
+
+        System.out.println("Вывод эпика 1,2,3,4:");
+        System.out.println(taskManager.getEpicById(epic1.getId()));
+        System.out.println(taskManager.getEpicById(epic2.getId()));
+        System.out.println(taskManager.getEpicById(epic3.getId()));
+        System.out.println(taskManager.getEpicById(epic4.getId()));
+        task1.setStatus(TaskStatus.DONE);
+        taskManager.updateTask(task1);
+        System.out.println("Обновленная задача 1:");
+        System.out.println(taskManager.getTaskById(task1.getId()));
+
+
+        epic1.setStatus(TaskStatus.DONE);
+        taskManager.updateEpic(epic1);
+        System.out.println("Обновленный эпик 1:");
+        System.out.println(taskManager.getEpicById(epic1.getId()));
+
+
+        subtask1.setStatus(TaskStatus.IN_PROGRESS);
+        taskManager.updateSubtask(subtask1);
+        System.out.println("Обновленная подзадача 1:");
+        System.out.println(taskManager.getSubtasksByEpicId(epic1.getId()).get(subtask1.getId()));
+
+
+        taskManager.deleteTaskById(task2.getId());
+        System.out.println("Удаление задачи 2. Все задачи после удаления:");
+        System.out.println(taskManager.getTaskById(task1.getId()));
+        System.out.println(taskManager.getTaskById(task2.getId()));
+
+
+        taskManager.deleteEpicById(epic3.getId());
+        System.out.println("Удаление эпика 3. Все эпики после удаления:");
+        System.out.println(taskManager.getEpicById(epic1.getId()));
+        System.out.println(taskManager.getEpicById(epic2.getId()));
+        System.out.println(taskManager.getEpicById(epic3.getId()));
+        System.out.println(taskManager.getEpicById(epic4.getId()));
+
+
+        taskManager.deleteSubtaskById(subtask4.getId());
+        System.out.println("Удаление подзадачи 4 из эпика 2. Подзадачи эпика 2 после удаления:");
+        System.out.println(taskManager.getSubtasksByEpicId(epic2.getId()));
     }
 }
 
