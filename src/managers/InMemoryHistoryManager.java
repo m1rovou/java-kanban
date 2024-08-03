d47071894daee5e18007d163eb238b50c69eb20a
АSit <shuvaev.a.m@mail.ru>
2024-08-03 21:07:15 +0300

    6-sprint/second

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -8,73 +8,79 @@ import java.util.List;
 import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final Map<Integer, Node> history;
+    private final Map<Integer, Node> history = new HashMap<>();
     private Node head;
     private Node tail;
 
-    public InMemoryHistoryManager() {
-        history = new HashMap<>();
-        head = null;
-        tail = null;
+    class Node {
+        Node next;
+        Task data;
+        Node prev;
+
+        Node(Task task) {
+            this.data = task;
+        }
     }
 
     @Override
     public List<Task> getHistory() {
         List<Task> historyList = new ArrayList<>();
-        if (!(history.isEmpty())) {
-            Node node = head;
-            for (int i = 0; i < history.size(); i++) {
-                historyList.add(node.getData());
-                node = node.getNext();
-            }
+        if (history.isEmpty()) {
+            return historyList;
+        }
+        Node node = head;
+        while (node != null) {
+            historyList.add(node.data);
+            node = node.next;
         }
         return historyList;
     }
 
     @Override
     public void add(Task task) {
-        if (task != null) {
-            remove(task.getId());
-            Node node = new Node(task);
-            linkLast(node);
-            history.put(task.getId(), node);
+        if (task == null) {
+            return;
         }
+        remove(task.getId());
+        Node node = new Node(task);
+        linkLast(node);
+        history.put(task.getId(), node);
     }
 
     @Override
     public void remove(int id) {
         Node node = history.get(id);
-        if (node != null) {
-            removeNode(node);
-            history.remove(id);
+        if (node == null) {
+            return;
         }
+        removeNode(node);
+        history.remove(id);
     }
 
     private void linkLast(Node node) {
-        if (history.isEmpty()) {
+        if (tail == null) {
             head = node;
         } else {
-            tail.setNext(node);
-            node.setPrev(tail);
+            tail.next = node;
+            node.prev = tail;
         }
         tail = node;
     }
 
     private void removeNode(Node node) {
-        Node prev = node.getPrev();
-        Node next = node.getNext();
-        if (prev == null && next == null) {
-            head = null;
-            tail = null;
-        } else if (prev == null) {
-            next.setPrev(null);
+        Node prev = node.prev;
+        Node next = node.next;
+
+        if (prev == null) {
             head = next;
-        } else if (next == null) {
-            prev.setNext(null);
+        } else {
+            prev.next = next;
+        }
+
+        if (next == null) {
             tail = prev;
         } else {
-            prev.setNext(next);
-            next.setPrev(prev);
+            next.prev = prev;
         }
     }
 }
